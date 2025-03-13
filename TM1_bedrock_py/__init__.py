e4a1bd1a923f708058f4367593a54c1b7e061490
eszter-b <buriseszter@gmail.com>
2025-03-13 10:10:03 +0100

    create log folder if not exists

--- TM1_bedrock_py/__init__.py
+++ TM1_bedrock_py/__init__.py
@@ -2,6 +2,13 @@ import logging.config
 import json
 import os
 
+# Define log directory and file
+log_dir = "logs"
+log_file = os.path.join(log_dir, "my_package.log")
+
+# Ensure the log directory exists
+os.makedirs(log_dir, exist_ok=True)
+
 # Get the path of the logging.json file
 log_config_path = os.path.join(os.path.dirname(__file__), "logging.json")
 
@@ -9,6 +16,13 @@ log_config_path = os.path.join(os.path.dirname(__file__), "logging.json")
 if os.path.exists(log_config_path):
     with open(log_config_path, "r") as f:
         log_config = json.load(f)
+
+    log_file = log_config["handlers"]["file"]["filename"]
+
+    # Ensure the log directory exists
+    log_dir = os.path.dirname(log_file)
+    os.makedirs(log_dir, exist_ok=True)
+
     logging.config.dictConfig(log_config)
 else:
     logging.basicConfig(level=logging.INFO)  # Fallback if JSON config is missing
