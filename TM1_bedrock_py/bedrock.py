68fbce9c5d1a1d33074b55653fcfa9fa983f3b5f
davidullmann271 <davidullmann271@gmail.com>
2025-03-20 14:18:11 +0100

    small bedrock fix for multiconnect

--- TM1_bedrock_py/bedrock.py
+++ TM1_bedrock_py/bedrock.py
@@ -180,6 +180,9 @@ def data_copy_intercube(
         ]
     """
 
+    if not target_tm1_service:
+        target_tm1_service = tm1_service
+
     utility.set_logging_level(logging_level=logging_level)
     basic_logger.info("Execution started.")
 
@@ -204,7 +207,7 @@ def data_copy_intercube(
     )
 
     target_metadata = utility.TM1CubeObjectMetadata.collect(
-        tm1_service=tm1_service,
+        tm1_service=target_tm1_service,
         cube_name=target_cube_name,
         metadata_function=target_metadata_function,
         collect_dim_element_identifiers=ignore_missing_elements,
@@ -254,9 +257,6 @@ def data_copy_intercube(
         transformer.dataframe_value_scale(dataframe=dataframe, value_function=value_function)
 
     transformer.dataframe_reorder_dimensions(dataframe=dataframe, cube_dimensions=target_metadata.get_cube_dims())
-
-    if not target_tm1_service:
-        target_tm1_service = tm1_service
 
     if clear_target:
         loader.clear_cube(
@@ -686,140 +686,7 @@ def load_tm1_cube_to_sql_table(
         **kwargs
 ) -> None:
     """
-    Copies data from a source cube to a target cube in TM1, with optional transformations, mappings,
-    and basic value scale.
 
-    Parameters:
-    ----------
-    tm1_service : Optional[Any]
-        TM1 service instance used to interact with the TM1 server.
-    data_mdx : Optional[str]
-        MDX query string for retrieving source data. Currently, this can be the only source
-    mdx_function : Optional[Callable[..., DataFrame]]
-        Function to execute an MDX query and return a DataFrame.
-    sql_engine : Optional[Any]
-        A sql connection engine that pandas.read_sql expects, preferably a SQLAlchemy engine.
-    sql_function : Optional[Callable[..., DataFrame]]
-        Function to execute a SQL query and return a DataFrame.
-    data_mdx_list : Optional[list[str]]
-        List of MDX queries for retrieving multiple data sets.
-    skip_zeros : Optional[bool], default=False
-        Whether to skip zero values when retrieving source data.
-    skip_consolidated_cells : Optional[bool], default=False
-        Whether to skip consolidated cells in the source data.
-    skip_rule_derived_cells : Optional[bool], default=False
-        Whether to skip rule-derived cells in the source data.
-    data_metadata_function : Optional[Callable[..., DataFrame]]
-        Function to retrieve metadata about the data source.
-    target_cube_name : Optional[str]
-        Name of the target cube where the data should be copied. If omitted, it will be set as the source cube name.
-    target_metadata_function: Optional[Callable[..., DataFrame]]
-            Function to retrieve metadata for the target cube.
-    mapping_steps : Optional[List[Dict]]
-        Steps for mapping data from source to target.
-    shared_mapping: Optional[Dict]
-        Information about the shared mapping data that can be used for the mapping steps.
-        Will generate a dataframe if any inputs are provided.
-        Has the same format as a singular mapping step
-    source_dim_mapping : Optional[dict]
-        Declaration of the dimensions present in the source dataframe, but not present in the target cube.
-        If there are such dimensions, these need to be specified, with for each dimension.
-        Rows will be filtered with the specified element, and then the dimension (column) will be dropped.
-    related_dimensions : Optional[dict]
-        Dictionary defining related dimensions for transformation. Source dimensions will be relabeled to the target
-        dimensions. Dimensionality and elements will stay unchanged.
-    target_dim_mapping : Optional[dict]
-        Declarations of the dimensions present in the target cube, but not in the data dataframe,
-        after all mapping steps. If there are such dimensions, these need to be specified, with an element for each
-        dimension. Dimensions (columns) will be added to the dataframe, and their values will be set uniformly.
-    value_function : Optional[Callable[..., Any]]
-        Function for transforming values before writing to the target cube.
-    clear_set_mdx_list : Optional[List[str]]
-        List of MDX queries to clear specific data areas in the target cube.
-    clear_target : Optional[bool], default=False
-        Whether to clear target before writing.
-    target_clear_set_mdx_list: Optional[List[str]]
-        List of MDX queries to clear specific data areas in the target cube.
-    clear_source: Optional[bool], default=False
-        Whether to clear the source after writing.
-    source_clear_set_mdx_list: Optional[List[str]]
-        List of MDX queries to clear specific data areas in the source cube.
-    async_write : bool, default=False
-        Whether to write data asynchronously. Currently, divides the data into 250.000 row chunks.
-    use_ti : bool, default=False
-        Whether to use TurboIntegrator (TI) for writing data.
-    use_blob : bool, default=False
-        Whether to use BLOB storage for data transfer.
-    increment : bool, default=False
-        Whether to increment existing values instead of replacing them in the cube.
-    sum_numeric_duplicates : bool, default=True
-        Whether to sum duplicate numeric values in the dataframe instead of overwriting them.
-    **kwargs
-        Additional keyword arguments for customization.
-
-    Returns:
-    -------
-    None
-        This function does not return anything; it writes data directly into TM1
-
-    Notes:
-    ------
-    - The function first collects metadata for the source data, and target cube.
-    - It then retrieves the dataframe with the provided mdx function and returns a dataframe.
-    - It adds filter dimensions with elements to the dataframe from the query metadata.
-    - It applies transformation and mapping logic.
-        Within this step, it also creates the shared and step-specific mapping dataframes using the provided mdx
-        function if necessary.
-    - It applies the provided value scale function
-    - It rearranges the columns to the expected shape of the target cube.
-    - If needed, it clears the target cube with the provided set MDX's
-    - Finally, it writes the processed data into the target cube in TM1.
-
-    Example of shared mapping:
-        {
-            "mapping_mdx": "////valid mdx////",
-            "mapping_metadata_function": mapping_metadata_function_name
-            "mapping_df": mapping_dataframe
-        }
-
-    Example of the 'mapping_steps' inside mapping_data::
-    -------
-
-        [
-            {
-                "method": "replace",
-                "mapping": {
-                    "dim1tochange": {"source": "target"},
-                    "dim2tochange": {"source3": "target3", "source4": "target4"}
-                }
-            },
-            {
-                "method": "map_and_replace",
-                "mapping_mdx": "////valid mdx////",
-                "mapping_metadata_function": mapping_metadata_function_name
-                "mapping_df": mapping_dataframe
-                "mapping_filter": {
-                    "dim": "element",
-                    "dim2": "element2"
-                },
-                "mapping_dimensions": {
-                    "dimname_to_change_values_of_in_source":"dim_to_change_the_values_with_in_mapping"
-                },
-                "relabel_dimensions": false
-            },
-            {
-                "method": "map_and_join",
-                "mapping_mdx": "////valid mdx////",
-                "mapping_metadata_function": mapping_metadata_function
-                "mapping_df": mapping_dataframe
-                "mapping_filter": {
-                    "dim": "element",
-                    "dim2": "element2"
-                },
-                "joined_columns": ["dim1tojoin", "dim2tojoin"],
-                "dropped_columns": ["dim1todrop", "dim2todrop"]
-            }
-        ]
     """
 
     utility.set_logging_level(logging_level=logging_level)
