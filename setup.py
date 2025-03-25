3bd40a02d9857935cc193a2039f2437578f3b810
eszter-b <buriseszter@gmail.com>
2025-03-25 10:49:53 +0100

    updated packaging files

--- setup.py
+++ setup.py
@@ -14,7 +14,19 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires=["TM1py", "mdxpy", "pandas"],
-    extras_require={"dev": ["pytest", "parametrize_from_file"]},
+    install_requires=[
+        "TM1py",
+        "mdxpy",
+        "pandas",
+        "json_logging",
+        "sqlalchemy"
+    ],
+    extras_require={"dev": [
+        "build",
+        "pytest",
+        "parametrize_from_file",
+        "apache_airflow",
+        "airflow_provider_tm1"
+    ]},
     python_requires=">=3.7"
 )
