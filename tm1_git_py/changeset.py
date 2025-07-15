878c06c86085860dd228bbc4b8b9b198fd098ce9
eszter-b <buriseszter@gmail.com>
2025-07-15 12:05:30 +0200

    sort and apply changeset

--- tm1_git_py/changeset.py
+++ tm1_git_py/changeset.py
@@ -1,4 +1,7 @@
+import re
 from typing import List, Dict, Any, TypeVar
+
+from requests import Response
 
 from model.cube import Cube, create_cube, update_cube, delete_cube
 from model.dimension import Dimension, create_dimension, update_dimension, delete_dimension
@@ -6,93 +9,155 @@ from model.process import Process, create_process, update_process, delete_proces
 from model.chore import Chore, create_chore, update_chore, delete_chore
 from TM1py import TM1Service
 
+from tm1_git_py.model.model import Model
+
 T = TypeVar('T', Cube, Dimension, Process, Chore)
 
 class Changeset:
+
+    model: Model
+
     def __init__(self):
-        self.added_cubes: List[Cube] = []
-        self.removed_cubes: List[str] = [] #List[Cube] = []
-        self.modified_cubes: List[Dict[str, Any]] = [] #{ 'old': Cube, 'new': Cube, 'changes': Cube}
 
-        self.added_dimensions: List[Dimension] = []
-        self.removed_dimensions: List[str] = [] #List[Dimension] = []
-        self.modified_dimensions: List[Dict[str, Any]] = []
+        self.added: List[T] = []
+        self.modified: List[Dict[T, Any]] = []
+        self.removed: List[T] = []
 
-        self.added_processes: List[Process] = []
-        self.removed_processes: List[str] = [] #List[Process] = []
-        self.modified_processes: List[Dict[str, Any]] = []
-
-        self.added_chores: List[Chore] = []
-        self.removed_chores: List[str] = [] #List[Chore] = []
-        self.modified_chores: List[Dict[str, Any]] = []
+        self.changes: List[str] = []
 
     @property
     def all_removed(self) -> List[str]:
-        return self.removed_cubes + self.removed_dimensions + self.removed_processes + self.removed_chores
+        return self.removed
 
     def has_changes(self) -> bool:
-        return any([
-            self.added_cubes, self.removed_cubes, self.modified_cubes,
-            self.added_dimensions, self.removed_dimensions, self.modified_dimensions,
-            self.added_processes, self.removed_processes, self.modified_processes,
-            self.added_chores, self.removed_chores, self.modified_chores
-        ])
+        return any([self.added, self.modified, self.removed])
+
 
     def __repr__(self):
-        changes = []
-        if self.added_cubes: changes.append(f"Added Cubes: {[c.name for c in self.added_cubes]}")
-        if self.removed_cubes: changes.append(f"Removed Cubes: {self.removed_cubes}")
-        if self.modified_cubes: changes.append(f"Modified Cubes: {[c['new'].name for c in self.modified_cubes]}")
 
-        if self.added_dimensions: changes.append(f"Added Dimensions: {[d.name for d in self.added_dimensions]}")
-        if self.removed_dimensions: changes.append(f"Removed Dimensions: {self.removed_dimensions}")
-        if self.modified_dimensions: changes.append(f"Modified Dimensions: {[d['new'].name for d in self.modified_dimensions]}")
+        if self.added: self.changes += [f"C  /{c.source_path}" for c in self.added]
+        if self.removed: self.changes += [f"D  /{c.source_path}" for c in self.removed]
+        if self.modified: self.changes += [f"U  /{c['new'].source_path}" for c in self.modified]
 
-        if self.added_processes: changes.append(f"Added Processes: {[p.name for p in self.added_processes]}")
-        if self.removed_processes: changes.append(f"Removed Processes: {self.removed_processes}")
-        if self.modified_processes: changes.append(f"Modified Processes: {[p['new'].name for p in self.modified_processes]}")
-
-        if self.added_chores: changes.append(f"Added Chores: {[c.name for c in self.added_chores]}")
-        if self.removed_chores: changes.append(f"Removed Chores: {self.removed_chores}")
-        if self.modified_chores: changes.append(f"Modified Chores: {[c['new'].name for c in self.modified_chores]}")
-
-        if not changes:
+        if not self.changes:
             return "No changes"
-        return "Changeset:\n" + "\n".join(changes)
+        else:
+            self.sort()
+        return "Changeset:\n" + "\n".join(self.changes)
 
     def apply(self, tm1_service: TM1Service) -> List[Any]:
         changes = []
+        self.validate()
 
         if self.has_changes():
-            if self.added_dimensions: changes.append(
-                create_dimension(tm1_service=tm1_service, dimension=d).url for d in self.added_dimensions)
-            if self.removed_dimensions: changes.append(
-                delete_dimension(tm1_service=tm1_service, dimension=d).url for d in self.removed_dimensions)
-            if self.modified_dimensions: changes.append(
-                update_dimension(tm1_service=tm1_service, dimension=d).url for d in self.modified_dimensions)
+            if self.added:
+                changes += [create_object(tm1_service=tm1_service, object_instance=a).url for a in self.added]
 
-            if self.added_cubes: changes.append(
-                create_cube(tm1_service=tm1_service, cube=c).url for c in self.added_cubes)
-            if self.removed_cubes: changes.append(
-                delete_cube(tm1_service=tm1_service, cube=c).url for c in self.removed_cubes)
-            if self.modified_cubes: changes.append(
-                update_cube(tm1_service=tm1_service, cube=c).url for c in self.modified_cubes)
+            if self.modified:
+                changes += [update_object(tm1_service=tm1_service, object_instance=m).url for m in self.modified]
 
-            if self.added_processes: changes.append(
-                create_process(tm1_service=tm1_service, process=p).url for p in self.added_processes)
-            if self.removed_processes: changes.append(
-                delete_process(tm1_service=tm1_service, process=p).url for p in self.removed_processes)
-            if self.modified_processes: changes.append(
-                update_process(tm1_service=tm1_service, process=p).url for p in self.modified_processes)
-
-            if self.added_chores: changes.append(
-                create_chore(tm1_service=tm1_service, chore=c).url for c in self.added_chores)
-            if self.removed_chores: changes.append(
-                delete_chore(tm1_service=tm1_service, chore=c).url for c in self.removed_chores)
-            if self.modified_chores: changes.append(
-                update_chore(tm1_service=tm1_service, chore=c).url for c in self.modified_chores)
+            if self.removed:
+                changes += [delete_object(tm1_service=tm1_service, object_instance=d).url for d in self.removed]
 
         return changes
 
+
+    def validate(self):
+        """validate changeset"""
+
+
     def sort(self):
-        """sort the dependency graph to apply changes in correct order"""
+        flag_precedence = {'C': 0, 'U': 1, 'D': 2}
+        object_precedence = {'dimensions': 0, 'cubes': 1, 'process': 2, 'chore': 3}
+
+        def __sort_changes(s: str):
+            changes_precedence = {'dimensions': 0, 'cubes': 1, 'process': 2, 'chore': 3}
+
+            flag = re.search(r'\A([UDC])', s).group(1)
+            obj_name = re.search(r'/\b(\w*)/', s).group(1)
+            source_path = s.split(obj_name)[1]
+
+            if flag == 'D':
+                changes_precedence = {'cubes': 0, 'dimensions': 1, 'chore': 2, 'process': 3}
+
+            key = (
+                flag_precedence.get(flag, 99),
+                changes_precedence.get(obj_name, 99),
+                source_path
+            )
+
+            return key
+
+        def __sort_on_source_path(s: T | Dict[T, Any]):
+
+            if isinstance(s, (Cube, Dimension, Chore, Process)):
+                s = s.source_path
+            else:
+                s = s["new"].source_path
+
+            obj_name = re.search(r'\A(\w*)/', s).group(1)
+            source_path = s.split(obj_name)[1]
+            key = (
+                object_precedence.get(obj_name, 99),
+                source_path
+            )
+
+            return key
+
+        if self.has_changes():
+            self.changes.sort(key=__sort_changes)
+            self.added.sort(key=__sort_on_source_path)
+            self.modified.sort(key=__sort_on_source_path)
+            if self.removed:
+                object_precedence = {'cubes': 0, 'dimensions': 1, 'chore': 2, 'process': 3}
+                self.removed.sort(key=__sort_on_source_path)
+
+
+def create_object(tm1_service: TM1Service, object_instance: T) -> Response:
+    if type(object_instance) is Dimension:
+        return create_dimension(tm1_service=tm1_service, dimension=object_instance)
+
+    elif type(object_instance) is Cube:
+        return create_cube(tm1_service=tm1_service, cube=object_instance)
+
+    elif type(object_instance) is Process:
+        return create_process(tm1_service=tm1_service, process=object_instance)
+
+    elif type(object_instance) is Chore:
+        return create_chore(tm1_service=tm1_service, chore=object_instance)
+
+    else: raise ValueError
+
+
+def delete_object(tm1_service: TM1Service, object_instance: T) -> Response:
+    if type(object_instance) is Cube:
+        return delete_cube(tm1_service=tm1_service, cube=object_instance)
+
+    elif type(object_instance) is Dimension:
+        return delete_dimension(tm1_service=tm1_service, dimension=object_instance)
+
+    elif type(object_instance) is Chore:
+        return delete_chore(tm1_service=tm1_service, chore=object_instance)
+
+    elif type(object_instance) is Process:
+        return delete_process(tm1_service=tm1_service, process=object_instance)
+
+    else:
+        raise ValueError
+
+
+def update_object(tm1_service: TM1Service, object_instance: Dict[T, Any]) -> Response:
+    if type(object_instance['new']) is Dimension:
+        return update_dimension(tm1_service=tm1_service, dimension=object_instance)
+
+    elif type(object_instance['new']) is Cube:
+        return update_cube(tm1_service=tm1_service, cube=object_instance)
+
+    elif type(object_instance['new']) is Process:
+        return update_process(tm1_service=tm1_service, process=object_instance)
+
+    elif type(object_instance['new']) is Chore:
+        return update_chore(tm1_service=tm1_service, chore=object_instance)
+
+    else:
+        raise ValueError
--- tm1_git_py/comparator.py
+++ tm1_git_py/comparator.py
@@ -20,28 +20,29 @@ class Comparator:
         changeset = Changeset()
 
         self._compare_object_lists(model1.cubes, model2.cubes,
-                                   changeset.added_cubes, changeset.removed_cubes,
-                                   changeset.modified_cubes,
+                                   changeset.added, changeset.removed,
+                                   changeset.modified,
                                    object_type_name="Cube",
                                    mode=mode)
 
         self._compare_object_lists(model1.dimensions, model2.dimensions,
-                                   changeset.added_dimensions, changeset.removed_dimensions,
-                                   changeset.modified_dimensions,
+                                   changeset.added, changeset.removed,
+                                   changeset.modified,
                                    object_type_name="Dimension",
                                    mode=mode)
 
         self._compare_object_lists(model1.processes, model2.processes,
-                                   changeset.added_processes, changeset.removed_processes,
-                                   changeset.modified_processes,
+                                   changeset.added, changeset.removed,
+                                   changeset.modified,
                                    object_type_name="Process",
                                    mode=mode)
 
         self._compare_object_lists(model1.chores, model2.chores,
-                                   changeset.added_chores, changeset.removed_chores,
-                                   changeset.modified_chores,
+                                   changeset.added, changeset.removed,
+                                   changeset.modified,
                                    object_type_name="Chore",
                                    mode=mode)
+        
         return changeset
 
     def _compare_object_lists(self,
@@ -63,7 +64,7 @@ class Comparator:
         if mode == 'full':
             for name, obj in old_map.items():
                 if name not in new_map:
-                    removed_list.append(obj.name)
+                    removed_list.append(obj)
 
         for name, new_obj in new_map.items():
             if name in old_map:
