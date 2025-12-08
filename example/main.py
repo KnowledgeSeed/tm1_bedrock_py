8f4df90615387705a03666dc927adb836dedc775
davidullmann271 <davidullmann271@gmail.com>
2025-12-08 16:10:37 +0100

    demo 3

--- example/main.py
+++ example/main.py
@@ -3,6 +3,18 @@ from TM1_bedrock_py import bedrock
 
 
 def tm1_bedrock_py_demo():
+
+    # source: Sales data -> on real existing employee-orgunit assignment -> Actual version, Sales cube
+    # we're interested how the data would look after company restructuring -> on Budget version
+
+    # helper mapping cube -> dimensions: version, employee, orgunit, measure
+    # different company structure ( employee-orgunit assignment ) on different versions (Actual, Budget)
+
+    # the task: sales cube query from source version (Actual)
+    #           version rewrite from source to target (Budget)
+    #           data restructuring with the help of the mapping cube, on version, employee
+    #           writing the transformed data back to the cube
+
     tm1_params = {
         "address": "dev.knowledgeseed.local",
         "port": 5379,
@@ -12,55 +24,67 @@ def tm1_bedrock_py_demo():
     }
     tm1_service = TM1Service(**tm1_params)
 
-    target_cube_name = "Sales Reporting"
     data_mdx = """
     SELECT
     NON EMPTY
-        {TM1FILTERBYLEVEL( {TM1SUBSETALL([Period].[Period])} , 0)}
+        {[Period].[Period].[202406]}
     ON COLUMNS,
     NON EMPTY
         {[Lineitem Sales].[Lineitem Sales].Members}
         * {TM1FILTERBYLEVEL( {TM1SUBSETALL([Currency].[Currency])} , 0)}
         * {TM1FILTERBYLEVEL( {TM1SUBSETALL([Product].[Product])} , 0)}
+        * {TM1FILTERBYLEVEL( {TM1SUBSETALL([Organization Unit].[Organization Unit])} , 0)}
+        * {TM1FILTERBYLEVEL( {TM1SUBSETALL([Employee].[Employee])} , 0)}
     ON ROWS
     FROM [Sales]
     WHERE (
         [Version].[Version].[Actual],
-        [Organization Unit].[Organization Unit].[Group],
-        [Employee].[Employee].[All Employee],
         [Measures Sales].[Measures Sales].[Corrected Value] )
     """
-    mapping_steps = [{
-        "method": "replace",
-        "mapping": {"Version": {"Actual": "Actual Reporting"}}
-    }]
-    source_dim_mapping = {
-        "Employee": "All Employee",
-        "Organization Unit": "Group"
-    }
-    related_dimensions = {
-        "Lineitem Sales": "Lineitem Sales Reporting"
-    }
+
+    mapping_steps = [
+        {
+            "method": "replace",
+            "mapping": {"Version": {"Actual": "Budget"}}
+        },
+        {
+            "method": "map_and_replace",
+            "mapping_mdx": """
+                SELECT
+                NON EMPTY 
+                    {TM1FILTERBYLEVEL( {TM1SUBSETALL([Employee].[Employee])} , 0)}
+                ON COLUMNS, 
+                NON EMPTY 
+                    {TM1FILTERBYLEVEL( {TM1SUBSETALL([Organization Unit].[Organization Unit])} , 0)} 
+                ON ROWS
+                FROM [Employee to Organization Unit]
+                WHERE (
+                    [Version].[Version].[Budget],
+                    [Measure Employee to Organization Unit].[Assign Flag]
+                )
+            """,
+            "mapping_dimensions": {"Organization Unit": "Organization Unit"}
+        }
+    ]
 
     clear_target = True
-    target_clear_set_mdx_list = ["{[Version].[Version].[Actual Reporting]}"]
+    target_clear_set_mdx_list = ["{[Version].[Version].[Budget]}", "{[Period].[Period].[202406]}"]
+
     logging_level = "DEBUG"
     use_blob = True
 
     try:
-        bedrock.data_copy_intercube( target_cube_name=target_cube_name,
-                                     tm1_service=tm1_service,
-                                     data_mdx=data_mdx,
-                                     mapping_steps=mapping_steps,
-                                     source_dim_mapping=source_dim_mapping,
-                                     related_dimensions=related_dimensions,
-                                     clear_target=clear_target,
-                                     target_clear_set_mdx_list=target_clear_set_mdx_list,
-                                     use_blob=use_blob,
-                                     logging_level=logging_level)
+        bedrock.data_copy(tm1_service=tm1_service,
+                          data_mdx=data_mdx,
+                          mapping_steps=mapping_steps,
+                          clear_target=clear_target,
+                          target_clear_set_mdx_list=target_clear_set_mdx_list,
+                          use_blob=use_blob,
+                          logging_level=logging_level)
     finally:
         tm1_service.logout()
 
 
 if __name__ == '__main__':
     tm1_bedrock_py_demo()
+
